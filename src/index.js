5f4f88955dec659c9cbf8a2d8edccc536d387b26
ChangRongKun <cccrk123@163.com>
2020-08-25 10:20:19 +0800

    webpack 项目中使用 vue 组件

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import $ from 'jquery'
 import './css/1.css'
 import './css/1.less'
 import './css/1.scss'
-// 导入Vue单文件组件
-import App from './components/App.vue'
 
 $(function () {
   $("li:odd").css('backgroundColor', 'pink');
@@ -15,3 +13,13 @@ class Person {
 }
 
 console.log(Person.info);
+
+//---------------------------------------
+import Vue from 'vue'
+// 导入Vue单文件组件
+import App from './components/App.vue'
+
+const vm = new Vue({
+  el: '#app',
+  render: h => h(App)
+});
